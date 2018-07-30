b286b4e009be1fd5f2adcc668c75baf4f787430b
Vitaliy Semenoff <deepy_dee@bk.ru>
2018-07-30 17:54:58 +0300

    Index.html with modal popups

--- app/js/common.js
+++ app/js/common.js
@@ -54,9 +54,34 @@ $(function() {
 		});
 		return false;
 	});
+
+	$('.map').magnificPopup({
+		type: 'iframe',
+		items: {
+				src:'https://yandex.ru/map-widget/v1/?um=constructor%3A2152ae03c9989425b575be008096f392c1c904896a0e33ae9c2142f9b06eccef&amp;source=constructor" width="769" height="510" frameborder="0"'
+		}
+});
+
+$('.entry-popup-link').magnificPopup({
+	type: 'inline'
+})
+
+$('.callback').magnificPopup({
+	type: 'inline'
+})
 	 
+	 
+$(window).scroll(function() {
+	if ($(this).scrollTop() > $(this).height()/2) {
+		$('.top').addClass('active');
+	} else {
+		$('.top').removeClass('active');
+	}
+});
 
-
+$('.top').click(function() {
+	$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+});
 
 });
 
